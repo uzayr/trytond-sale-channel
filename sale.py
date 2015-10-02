f6e34b898a4e08f638e8a9f648cbf356f485b1b0
Tarun Bhardwaj <mailme@tarunbhardwaj.com>
2015-10-02 14:29:57 +0530

    Added method to process sale order to channel state

--- sale.py
+++ sale.py
@@ -288,3 +288,23 @@ class Sale:
                 default['channel'] = cls.default_channel()
 
         return super(Sale, cls).copy(sales, default=default)
+
+    def process_to_channel_state(self, channel_state):
+        """
+        Process the sale in tryton based on the state of order
+        when its imported from channel
+
+        :param channel_state: State on external channel the order was imported
+        """
+        Sale = Pool().get('sale.sale')
+
+        data = self.channel.get_tryton_action(channel_state)
+
+        if data['action'] in ['process_manually', 'process_automatically']:
+            Sale.quote([self])
+            Sale.confirm([self])
+
+        if data['action'] == 'process_automatically':
+            Sale.process([self])
+
+        # TODO: Handle import as past action for magento state
