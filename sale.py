a9cc8d7e8bd4906b1bb70d6bd1c513a0cb6a3b21
Priyanka Rani <priyanka.rani@fulfil.io>
2015-11-25 18:09:48 -0600

    Do not copy channel_identifier while copying sale and sale line

--- sale.py
+++ sale.py
@@ -301,6 +301,8 @@ class Sale:
             if not sale.check_create_access(True):
                 default['channel'] = cls.default_channel()
 
+        default['channel_identifier'] = None
+
         return super(Sale, cls).copy(sales, default=default)
 
     def process_to_channel_state(self, channel_state):
@@ -350,6 +352,18 @@ class SaleLine:
             "duplicate_order_line":
                 'Sale Line with Order Item ID "%s" already exists',
         })
+
+    @classmethod
+    def copy(cls, lines, default=None):
+        """
+        Duplicating records
+        """
+        if default is None:
+            default = {}
+
+        default['channel_identifier'] = None
+
+        return super(SaleLine, cls).copy(lines, default=default)
 
     @classmethod
     def validate(cls, lines):
